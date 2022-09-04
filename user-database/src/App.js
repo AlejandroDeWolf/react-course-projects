61c106bf46c18ab1a9b9cf090fe514671644f955
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-04 15:13:23 +0200

    feat: show new user in list when added + error modal

--- user-database/src/App.js
+++ user-database/src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
+import UsersList from "./components/Users/UsersList";
 import AddUser from "./components/Users/AddUser";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      // we use the previous state to make sure we don't overwrite the previous state
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
