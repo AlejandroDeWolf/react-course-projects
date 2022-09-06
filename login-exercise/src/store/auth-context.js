5c595c10434606aab91392c6807d8147ec6427e7
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-06 18:40:25 +0200

    refactor: usage of central authentication to use in every component by using useContext instead of crating a props chain

--- login-exercise/src/store/auth-context.js
+++ login-exercise/src/store/auth-context.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // this is a function that returns a context object
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1"); // 1 is just a dummy value, 1 means logged in and 0 means logged out
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
