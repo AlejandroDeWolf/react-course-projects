9dfb9fff8e762c37d0f927eb8e027ad4824cc600
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-09 15:39:26 +0200

    feat: display header with image + styling

---
+++ 5-food-ordering-app/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import headerImage from "../../assets/header-image.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+
+      <div className={classes["main-image"]}>
+        <img src={headerImage} alt="A table full of delicious food!" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
