727bbefe05036f00bb094c48a0c53788c3a5befb
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-09 17:46:48 +0200

    feat: add input form to add meals to cart

---
+++ 5-food-ordering-app/src/components/UI/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  );
+};
+
+// we are using the spread operator to pass all the props to the input element
+
+export default Input;
