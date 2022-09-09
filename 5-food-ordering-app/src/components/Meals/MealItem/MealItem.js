74884a8fc3b88f65d63eb4890963a370abb9619b
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-09 17:30:02 +0200

    feat: display (hard coded) meals

---
+++ 5-food-ordering-app/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  // dollar sign is a convention for variables that hold JSX
+  // to fixed is a method that converts a number to a string
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <button>+ Add</button>
+      </div>
+    </li>
+  );
+};
+
+export default MealItem;
