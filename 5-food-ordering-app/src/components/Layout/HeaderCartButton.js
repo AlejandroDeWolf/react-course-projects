05319b316a731247674a3a30d4f5954969de58d3
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-10 16:24:05 +0200

    feat: cart bump animation when a meal is added

--- 5-food-ordering-app/src/components/Layout/HeaderCartButton.js
+++ 5-food-ordering-app/src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   // now this component will be re-rendered whenever the cart context changes, and it changes when we do it in the CartProvider component
   const cartCtx = useContext(CartContext);
 
+  const { items } = cartCtx;
+
   // reduce is a built-in array method that allows us to transform an array into a single value
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // cleanup function that runs before the next useEffect function runs
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
