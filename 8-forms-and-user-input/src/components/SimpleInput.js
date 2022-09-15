6c904f10365836502abaa4e68072a6a085dccd24
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-09-15 17:31:12 +0200

    feat: check for input value in console

--- 8-forms-and-user-input/src/components/SimpleInput.js
+++ 8-forms-and-user-input/src/components/SimpleInput.js
@@ -1,9 +1,31 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  // we use ref when we want to access the value of an element once
+  const nameInputRef = useRef();
+  // we use state when we want to re-render the component when the value changes, thus every keystroke or when we want to clear the input
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangedHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangedHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
