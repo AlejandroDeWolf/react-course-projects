c15e7e4e7beb580ba14522e65449bb7156cfb37b
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-02 15:56:56 +0100

    feat: link to details page by passing id in url

--- 12-react-router/frontend/src/pages/Events.js
+++ 12-react-router/frontend/src/pages/Events.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Events = () => {
-  return <div>Events</div>;
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Programming for everyone",
+  },
+  {
+    id: "e2",
+    title: "Networking for introverts",
+  },
+];
+
+const EventsPage = () => {
+  return (
+    <>
+      <div>Events</div>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
-export default Events;
+export default EventsPage;
