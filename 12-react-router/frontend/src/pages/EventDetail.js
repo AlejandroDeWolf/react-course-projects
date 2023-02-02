714fd9be1675c1dacba4ca25e77d6d681b10c540
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-02 15:58:39 +0100

    feat: display event id on corresponding page

--- 12-react-router/frontend/src/pages/EventDetail.js
+++ 12-react-router/frontend/src/pages/EventDetail.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const EventDetailPage = () => {
-  return <div>EventDetail</div>;
+  const params = useParams();
+
+  return (
+    <div>
+      <h1>Event Detail Page</h1>
+      <p>{params.eventId}</p>
+    </div>
+  );
 };
 
 export default EventDetailPage;
