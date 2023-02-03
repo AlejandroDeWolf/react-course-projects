10103e060dbb3d024da36c9d42fbe83d0a6b1647
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-03 20:47:43 +0100

    feat: show dynamic error page depending on error status code

--- 12-react-router/frontend/src/pages/Error.js
+++ 12-react-router/frontend/src/pages/Error.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
 
 const ErrorPage = () => {
-  return <h1>An error ocurred</h1>;
+  const error = useRouteError();
+
+  let title = "An error ocurred!";
+  let message = "Page Not Found";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Page Not Found";
+    message = "Could not find resource or page";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 };
 
 export default ErrorPage;
--- 12-react-router/frontend/src/pages/Events.js
+++ 12-react-router/frontend/src/pages/Events.js
@@ -15,7 +15,9 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    throw new Error("Network response was not ok");
+    throw new Response(JSON.stringify({ message: "Failed to fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
   }
