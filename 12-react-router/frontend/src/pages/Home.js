82273bfbcf9b498e1c9e67faad8bb0d82e78ae2d
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-04 18:26:14 +0100

    feat: add a bit of content on homepage

--- 12-react-router/frontend/src/pages/Home.js
+++ 12-react-router/frontend/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import PageContent from "../components/PageContent";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <PageContent title="Welcome!">
+      <p>
+        Browse all events or create your own. You can also edit or delete
+        existing events.
+      </p>
+    </PageContent>
+  );
 };
 
 export default HomePage;
