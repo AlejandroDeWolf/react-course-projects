5c68a38222d6b39457a6ee9b69515f55be0c7c57
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-08 11:49:38 +0100

    test: test getServerSideProps function

--- 15-next-introduction/pages/index.js
+++ 15-next-introduction/pages/index.js
@@ -23,14 +23,33 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// this will run on the server, after deployment, the page will be pre-rendered with the data we fetch
+// only use this when we have a lot of incoming requests
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+
+//   // not need to revalidate, because this runs for every incoming request
+// }
+
 // this way we can pre-render the page with the data we fetch, the source code of the page will be generated at build time
 export async function getStaticProps() {
   // fetch data from an API
+
   return {
     props: {
       meetups: DUMMY_MEETUPS,
     },
-    revalidate: 1,
+    // every 10 seconds, the page will be re-generated
+    revalidate: 10,
   };
 }
 
