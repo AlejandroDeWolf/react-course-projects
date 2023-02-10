0770525847525e27a7f7df2171a42425a059c139
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-10 16:27:10 +0100

    test: test with mock value

--- 16-testing-intro/src/components/Async.test.js
+++ 16-testing-intro/src/components/Async.test.js
@@ -5,6 +5,12 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders posts if request succeeds", async () => {
+    // we use jest to mock the fetch function
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
+
     render(<Async />);
 
     // add an assertion here
