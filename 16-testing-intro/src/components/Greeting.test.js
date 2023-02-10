b66e441b27a599499fa92aa441ed667d66fe5898
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-10 11:54:07 +0100

    test: write test to check if content is conditionally rendered correctly

--- 16-testing-intro/src/components/Greeting.test.js
+++ 16-testing-intro/src/components/Greeting.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
-  test("renders Hello World as a text", () => {
+  test("renders 'Hello World!' as a text", () => {
     // Arrange
     render(<Greeting />);
 
@@ -12,5 +14,33 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders 'It's good to see you!' if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const outputElement = screen.getByText("It's good to see you!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render 'It's good to see you!' if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // query* functions will return the element or null if it cannot be found
+    const outputElement = screen.queryByText("It's good to see you!");
+    expect(outputElement).toBeNull();
   });
 });
