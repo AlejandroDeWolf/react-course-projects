1777e228e0e95ef18accd8bde3283d5f4abd11ca
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-12 12:27:10 +0100

    feat: use context api instead of prop drilling

--- 18-typescript-and-react/src/components/Todos.tsx
+++ 18-typescript-and-react/src/components/Todos.tsx
@@ -1,20 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import TodoItem from "./TodoItem";
-import Todo from "../models/todo";
-
+import { TodosContext } from "../store/todos-context";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[]; removeTodo: (id: string) => void }> = (
-  props
-) => {
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
   return (
     <ul className={classes.todos}>
-      {props.items.map((todo) => (
+      {todosCtx.items.map((item) => (
         <TodoItem
-          key={todo.id}
-          text={todo.text}
-          removeTodo={props.removeTodo.bind(null, todo.id)}
+          key={item.id}
+          text={item.text}
+          removeTodo={todosCtx.removeTodo.bind(null, item.id)}
         />
       ))}
     </ul>
