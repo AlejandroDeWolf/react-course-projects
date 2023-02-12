d5de67a005088ab4a03ff7286bbf841db63362a1
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-12 11:36:05 +0100

    refactor: create seperate todo component

--- 18-typescript-and-react/src/components/Todos.tsx
+++ 18-typescript-and-react/src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <TodoItem key={todo.id} text={todo.text} />
       ))}
     </ul>
   );
