001aa2bdae739e332f08db1f43567c6a0cfed456
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2023-02-12 11:52:42 +0100

    feat: add todos with form and create style modules

--- 18-typescript-and-react/src/components/NewTodo.tsx
+++ 18-typescript-and-react/src/components/NewTodo.tsx
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 
-const NewTodo = () => {
+import classes from "./NewTodo.module.css";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null); // null is the initial value
 
   const submitHandler = (event: React.FormEvent) => {
@@ -13,10 +15,12 @@ const NewTodo = () => {
       // throw an error
       return;
     }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
       <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
